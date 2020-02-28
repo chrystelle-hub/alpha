e6199fe07ccee84877420b0ecfc9dc457a268f97
chrystelle-hub <chrystelle.damide@gmail.com>
2020-02-28 09:54:00 +0100

    entité entreprise

---
+++ APISymfony/src/Entity/Entreprise.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EntrepriseRepository")
+ */
+class Entreprise
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $raison_social;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $secteur_activite;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $code_postal;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $departement;
+
+    /**
+     * @ORM\Column(type="string", length=16)
+     */
+    private $tel;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $historique_modif;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getRaisonSocial(): ?string
+    {
+        return $this->raison_social;
+    }
+
+    public function setRaisonSocial(string $raison_social): self
+    {
+        $this->raison_social = $raison_social;
+
+        return $this;
+    }
+
+    public function getSecteurActivite(): ?string
+    {
+        return $this->secteur_activite;
+    }
+
+    public function setSecteurActivite(string $secteur_activite): self
+    {
+        $this->secteur_activite = $secteur_activite;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?int
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(int $code_postal): self
+    {
+        $this->code_postal = $code_postal;
+
+        return $this;
+    }
+
+    public function getDepartement(): ?string
+    {
+        return $this->departement;
+    }
+
+    public function setDepartement(string $departement): self
+    {
+        $this->departement = $departement;
+
+        return $this;
+    }
+
+    public function getTel(): ?string
+    {
+        return $this->tel;
+    }
+
+    public function setTel(string $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getHistoriqueModif(): ?string
+    {
+        return $this->historique_modif;
+    }
+
+    public function setHistoriqueModif(string $historique_modif): self
+    {
+        $this->historique_modif = $historique_modif;
+
+        return $this;
+    }
+}
