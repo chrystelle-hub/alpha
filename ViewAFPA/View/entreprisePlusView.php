868696b44c753a542ed895915877def7e7def183
antoine <toptonio56@hotmail.fr>
2020-03-09 10:09:25 +0100

    VueEntreprisePlus + css

---
+++ ViewAFPA/View/entreprisePlusView.php
@@ -0,0 +1,396 @@
+<?php
+require_once ("../View/template2.php"); ?>
+<div class="contenair_relative">
+    <div class="contenair_photo photo_2">
+    </div>
+    <div class="info_entreprise">
+        <div class="entreprise_with">
+            <h2>Dylan Entreprise</h2>
+            <label>56860 Erdeven 43 rue des asperges</label>
+            <label>DylanEntrepriseAssist@gmail.com</label>
+            <label>06 95 84 21 56</label>
+        </div>
+
+        <div class="info_sup_enter">
+
+            <div class="contenair_stat_entre info_cool2">
+                <div class="stat_entreprise info_cool">
+                    <i class="fas fa-globe"></i>
+                </div>
+            </div>
+
+            <div class="contenair_stat_entre info_cool2">
+                <div class="stat_entreprise info_cool">
+                    <i class="fas fa-briefcase"></i>
+                </div>
+            </div>
+
+            <div class="contenair_stat_entre info_cool2">
+                <div class="stat_entreprise info_cool">
+                    <i class="fas fa-map-marked-alt"></i>
+                </div>
+            </div>
+
+        </div>
+
+
+
+        <div class="option_button">
+            <div class="postuler">
+                <i class="fas fa-edit"></i>
+                <label>Postuler</label>
+            </div>
+            <div class="contact_form">
+                <i class="fas fa-user-plus"></i>
+                <label>Contact</label>
+            </div>
+            <div class="entreprise_form">
+                <i class="fas fa-user-plus"></i>
+                <label>entreprise</label>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="entreprise_contenair">
+    <div class="entreprise place_entre">
+        <div class="button_entreprise">
+
+            <div id="moyens_js">
+                <label>Moyens</label>
+            </div>
+
+            <div id="reponse_js">
+                <label>Réponse</label>
+            </div>
+
+        </div>
+
+        <div id="info_entreprise_js" class=" info_entreprise_ul cache_entreprise">
+            <ul class="label_info">
+                <li>Lettre</li>
+                <li>Email</li>
+                <li>Telephone</li>
+                <li>sur place</li>
+            </ul>
+            <ul class="chiffre_info">
+                <li>12</li>
+                <li>34</li>
+                <li>25</li>
+                <li>7</li>
+            </ul>
+        </div>
+
+        <div id="info_entreprise2_js" class=" info_entreprise_ul cache_entreprise">
+            <ul class="label_info">
+                <li>En attente</li>
+                <li>Entretien en attente de réponse</li>
+                <li>Refusé après entretien</li>
+                <li>Refusé sans entretien</li>
+                <li>Accepté</li>
+            </ul>
+            <ul class="chiffre_info">
+                <li>12</li>
+                <li>34</li>
+                <li>25</li>
+                <li>7</li>
+                <li>8</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="contenair_stat_plus">
+        <div class="stat_contenaire">
+            <div class="stat_1">
+                <div class="stat_classique">
+                    <p>100</p>
+                </div>
+                <label>Nombre de candidature</label>
+            </div>
+            <div class="stat_1">
+                <div class="stat_classique">
+                    <p>100</p>
+                </div>
+                <label>Candidature accepté</label>
+            </div>
+            <div class="stat_1">
+                <div class="stat_classique">
+                    <p>100</p>
+                </div>
+                <label>Candidature refusé</label>
+            </div>
+        </div>
+
+        <div class="stat_contenaire">
+            <div class="stat_1">
+                <div class="stat_classique">
+                    <p>100</p>
+                </div>
+                <label>Taux d'acceptation</label>
+            </div>
+            <div class="stat_1">
+                <div class="stat_classique">
+                    <p>100</p>
+                </div>
+                <label>Candidature attente</label>
+            </div>
+            <div class="stat_1">
+                <div class="stat_classique">
+                    <p>100</p>
+                </div>
+                <label>Recommannder</label>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="mes_infos">
+        <h2>Contact</h2>
+        <div class="contenair_user anti_box">
+
+
+            <div id="contenair_contact"  class="ligne_contact dylan_js">
+                   <div  class="name_ligne_contact">
+                       <h3>Dylan le craver</h3>
+                       <label>Fonction</label>
+                       <i class="fas fa-location-arrow"></i>
+                   </div>
+                    <div id="modif" class="info_ligne_contact cache_info" >
+                        <div class="icone_user">
+                            <ul>
+                                <li><i class="fab fa-linkedin"></i></li>
+                                <li><i class="fas fa-phone-square-alt"></i></li>
+                                <li><i class="fas fa-envelope-square"></i></li>
+                                <li><i class="fas fa-caret-square-down"></i></li>
+                            </ul>
+                        </div>
+                        <div class="icone_text">
+                            <ul>
+                                <li><a href="https://fr.linkedin.com/">https://fr.linkedin.com/</a></li>
+                                <li>06 85 87 32 15</li>
+                                <li>dylanLeCraver56@gmail.com</li>
+                                <li>Chef de chantier</li>
+                            </ul>
+                        </div>
+                    </div>
+            </div>
+
+
+            <div id="contenair_contact"  class="ligne_contact dylan_js">
+                <div  class="name_ligne_contact">
+                    <h3>Dylan le craver</h3>
+                    <label>Fonction</label>
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                <div id="modif" class="info_ligne_contact cache_info" >
+                    <div class="icone_user">
+                        <ul>
+                            <li><i class="fab fa-linkedin"></i></li>
+                            <li><i class="fas fa-phone-square-alt"></i></li>
+                            <li><i class="fas fa-envelope-square"></i></li>
+                            <li><i class="fas fa-caret-square-down"></i></li>
+                        </ul>
+                    </div>
+                    <div class="icone_text">
+                        <ul>
+                            <li><a href="https://fr.linkedin.com/">https://fr.linkedin.com/</a></li>
+                            <li>06 85 87 32 15</li>
+                            <li>dylanLeCraver56@gmail.com</li>
+                            <li>Chef de chantier</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+            <div id="contenair_contact"  class="ligne_contact dylan_js">
+                <div  class="name_ligne_contact">
+                    <h3>Dylan le craver</h3>
+                    <label>Fonction</label>
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                <div id="modif" class="info_ligne_contact cache_info" >
+                    <div class="icone_user">
+                        <ul>
+                            <li><i class="fab fa-linkedin"></i></li>
+                            <li><i class="fas fa-phone-square-alt"></i></li>
+                            <li><i class="fas fa-envelope-square"></i></li>
+                            <li><i class="fas fa-caret-square-down"></i></li>
+                        </ul>
+                    </div>
+                    <div class="icone_text">
+                        <ul>
+                            <li><a href="https://fr.linkedin.com/">https://fr.linkedin.com/</a></li>
+                            <li>06 85 87 32 15</li>
+                            <li>dylanLeCraver56@gmail.com</li>
+                            <li>Chef de chantier</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+            <div id="contenair_contact"  class="ligne_contact dylan_js">
+                <div  class="name_ligne_contact">
+                    <h3>Paul</h3>
+                    <label>Fonction</label>
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                <div id="modif" class="info_ligne_contact cache_info" >
+                    <div class="icone_user">
+                        <ul>
+                            <li><i class="fab fa-linkedin"></i></li>
+                            <li><i class="fas fa-phone-square-alt"></i></li>
+                            <li><i class="fas fa-envelope-square"></i></li>
+                            <li><i class="fas fa-caret-square-down"></i></li>
+                        </ul>
+                    </div>
+                    <div class="icone_text">
+                        <ul>
+                            <li><a href="https://fr.linkedin.com/">https://fr.linkedin.com/</a></li>
+                            <li>06 85 87 32 15</li>
+                            <li>dylanLeCraver56@gmail.com</li>
+                            <li>Chef de chantier</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="contenair_contact"  class="ligne_contact dylan_js">
+                <div  class="name_ligne_contact">
+                    <h3>Jean Marie</h3>
+                    <label>Fonction</label>
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                <div id="modif" class="info_ligne_contact cache_info" >
+                    <div class="icone_user">
+                        <ul>
+                            <li><i class="fab fa-linkedin"></i></li>
+                            <li><i class="fas fa-phone-square-alt"></i></li>
+                            <li><i class="fas fa-envelope-square"></i></li>
+                            <li><i class="fas fa-caret-square-down"></i></li>
+                        </ul>
+                    </div>
+                    <div class="icone_text">
+                        <ul>
+                            <li><a href="https://fr.linkedin.com/">https://fr.linkedin.com/</a></li>
+                            <li>06 85 87 32 15</li>
+                            <li>dylanLeCraver56@gmail.com</li>
+                            <li>Chef de chantier</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="contenair_contact"  class="ligne_contact dylan_js">
+                <div  class="name_ligne_contact">
+                    <h3>Dylan le craver</h3>
+                    <label>Fonction</label>
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                <div id="modif" class="info_ligne_contact cache_info" >
+                    <div class="icone_user">
+                        <ul>
+                            <li><i class="fab fa-linkedin"></i></li>
+                            <li><i class="fas fa-phone-square-alt"></i></li>
+                            <li><i class="fas fa-envelope-square"></i></li>
+                            <li><i class="fas fa-caret-square-down"></i></li>
+                        </ul>
+                    </div>
+                    <div class="icone_text">
+                        <ul>
+                            <li><a href="https://fr.linkedin.com/">https://fr.linkedin.com/</a></li>
+                            <li>06 85 87 32 15</li>
+                            <li>dylanLeCraver56@gmail.com</li>
+                            <li>Chef de chantier</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="contenair_contact"  class="ligne_contact">
+                <div  class="name_ligne_contact">
+                    <h3>Dylan le craver</h3>
+                    <label>Fonction</label>
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                <div id="modif" class="info_ligne_contact cache_info" >
+                    <div class="icone_user">
+                        <ul>
+                            <li><i class="fab fa-linkedin"></i></li>
+                            <li><i class="fas fa-phone-square-alt"></i></li>
+                            <li><i class="fas fa-envelope-square"></i></li>
+                            <li><i class="fas fa-caret-square-down"></i></li>
+                        </ul>
+                    </div>
+                    <div class="icone_text">
+                        <ul>
+                            <li><a href="https://fr.linkedin.com/">https://fr.linkedin.com/</a></li>
+                            <li>06 85 87 32 15</li>
+                            <li>dylanLeCraver56@gmail.com</li>
+                            <li>Chef de chantier</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+
+    voletProfil = true;
+    voletReponse = true;
+
+    function ouvertureMenuSetting(){
+
+        if(voletProfil){
+            voletProfil = false;
+            document.getElementById("info_entreprise_js").classList.remove("cache_entreprise");
+            document.getElementById("info_entreprise_js").classList.add("ouvert_entreprise");
+            document.getElementById("info_entreprise2_js").classList.remove("ouvert_entreprise");
+            document.getElementById("info_entreprise2_js").classList.add("cache_entreprise");
+            document.getElementById("moyens_js").classList.add("other_color");
+            document.getElementById("reponse_js").classList.remove("other_color");
+            voletReponse = true;
+        }
+        else{
+            voletProfil = true;
+            document.getElementById("info_entreprise_js").classList.remove("ouvert_entreprise");
+            document.getElementById("info_entreprise_js").classList.add("cache_entreprise");
+            document.getElementById("info_entreprise2_js").classList.remove("ouvert_entreprise");
+            document.getElementById("info_entreprise2_js").classList.add("cache_entreprise");
+            document.getElementById("moyens_js").classList.remove("other_color");
+            document.getElementById("reponse_js").classList.remove("other_color");
+        }
+    }
+
+    function ouvertureReponse(){
+
+        if(voletReponse){
+            voletReponse = false;
+            document.getElementById("info_entreprise2_js").classList.remove("cache_entreprise");
+            document.getElementById("info_entreprise2_js").classList.add("ouvert_entreprise");
+            document.getElementById("info_entreprise_js").classList.remove("ouvert_entreprise");
+            document.getElementById("info_entreprise_js").classList.add("cache_entreprise");
+            document.getElementById("reponse_js").classList.add("other_color");
+            document.getElementById("moyens_js").classList.remove("other_color");
+            voletProfil = true;
+        }
+        else{
+            voletReponse = true;
+            document.getElementById("info_entreprise2_js").classList.remove("ouvert_entreprise");
+            document.getElementById("info_entreprise2_js").classList.add("cache_entreprise");
+            document.getElementById("info_entreprise_js").classList.remove("ouvert_entreprise");
+            document.getElementById("info_entreprise_js").classList.add("cache_entreprise");
+            document.getElementById("reponse_js").classList.remove("other_color");
+            document.getElementById("moyens_js").classList.remove("other_color");
+        }
+    }
+
+
+    var varFermetureSetting = document.getElementById("moyens_js");
+    var varOuvertureReponse = document.getElementById("reponse_js");
+    varFermetureSetting.addEventListener("click",ouvertureMenuSetting);
+    varOuvertureReponse.addEventListener("click",ouvertureReponse);
+
+
+
+</script>
+
